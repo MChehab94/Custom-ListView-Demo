e1b6c2ffcfb8e01c2109f541c98fb4a53bc1279a
Mohamad <lemirmohammad.shehab@lau.edu>
2017-11-05 17:41:15 +0200

    Added broadcast receiver for AsyncTask, implemented onScrollListener for listView and add 10 more items

--- app/src/main/java/mchehab/com/customlistviewimage/MainActivity.java
+++ app/src/main/java/mchehab/com/customlistviewimage/MainActivity.java
@@ -1,8 +1,19 @@
 package mchehab.com.customlistviewimage;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.AsyncTask;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AbsListView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -10,18 +21,93 @@ import com.google.gson.reflect.TypeToken;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ListViewAdapter listViewAdapter;
+
+    private List<Person> listPerson;
+    private List<Person> listCompleteData;
+
+    private ProgressBar progressBar;
+    private AsyncTaskWait asyncTaskWait;
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            addMoreItems();
+            Log.d("asyncstatus", "status = " + asyncTaskWait.getStatus().name());
+        }
+    };
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter("result");
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause(){
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
+        super.onPause();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = findViewById(R.id.listView);
-        listView.setAdapter(new ListViewAdapter(this, readListFromFile()));
+        listView = findViewById(R.id.listView);
+        listCompleteData = readListFromFile();
+        listPerson = new ArrayList<>(listCompleteData.subList(0, 10));
+
+        listViewAdapter = new ListViewAdapter(this, listPerson);
+        listView.setAdapter(listViewAdapter);
+        setListViewFooter();
+        setListOnScrollListener();
+    }
+
+    private void setListViewFooter(){
+        View view = LayoutInflater.from(this).inflate(R.layout.footer_listview_progressbar, null);
+        progressBar = view.findViewById(R.id.progressBar);
+        listView.addFooterView(progressBar);
+    }
+
+    private void setListOnScrollListener(){
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                if(scrollState == SCROLL_STATE_IDLE && listView.getLastVisiblePosition() ==
+                        listPerson.size()){
+                    if(asyncTaskWait == null || asyncTaskWait.getStatus() != AsyncTask.Status
+                            .RUNNING){
+                        progressBar.setVisibility(View.VISIBLE);
+                        asyncTaskWait = new AsyncTaskWait(new WeakReference<Context>(MainActivity
+                                .this));
+                        asyncTaskWait.execute();
+                    }
+                }
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {}
+        });
+    }
+
+    private void addMoreItems(){
+        int size = listPerson.size();
+        for(int i=1;i<=10;i++){
+            if((size + i) < listCompleteData.size()){
+                listPerson.add(listCompleteData.get(size + i));
+            }
+        }
+        listViewAdapter.notifyDataSetChanged();
+        progressBar.setVisibility(View.GONE);
     }
 
     private List<Person> readListFromFile(){
